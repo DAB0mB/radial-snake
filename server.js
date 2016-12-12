d143037f20ef9e505aff2b495f285bda3a2b11f5
DAB0mB <emanor6@gmail.com>
2016-12-11 23:35:29 -0200

    Step 1.2: Add server basis

---
+++ server.js
@@ -0,0 +1,36 @@
+const Hapi = require("hapi");
+
+let port = 8000;
+
+// Initialize a new server
+let server = new Hapi.Server();
+
+server.connection({ port: process.env.PORT || port });
+
+// Report each response made
+server.ext("onPreResponse", (req, rep) => {
+  let res = req.response;
+
+  console.log("Outcoming response:");
+  console.log(`in: ${new Date}`);
+  console.log(`to: ${req.info.remoteAddress}`);
+  console.log(`method: ${req.method}`);
+  console.log(`url: ${req.url.path}`);
+  console.log(`status: ${res.statusCode || res.output.statusCode}`);
+  console.log();
+
+  rep.continue();
+});
+
+// Print message once started
+server.start((err) => {
+  if (err) throw err;
+
+  console.log();
+  console.log("---------- -------- ------ ---- --");
+  console.log("----- ---- --- -- -");
+  console.log(`Server running at ${port}`);
+  console.log("----- ---- --- -- -");
+  console.log("---------- -------- ------ ---- --");
+  console.log();
+});
