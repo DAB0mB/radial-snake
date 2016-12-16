315b2ed97ab5030f7d9ef3c65d9e0263d51c7f97
DAB0mB <emanor6@gmail.com>
2016-12-15 23:33:57 -0200

    Step 2.15: Set test screen as the initial screen

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   let game = new Engine.Game(document.getElementById("gameCanvas"), false);
+  game.changeScreen(TestScreen);
   game.play();
 });
