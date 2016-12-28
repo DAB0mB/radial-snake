90ce0abeb4d7293cc64df248d1da4947a7f2e458
DAB0mB <emanor6@gmail.com>
2016-12-28 00:18:47 -0200

    Step 3.5: Set splash screen as the initial game screen

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   let game = new Engine.Game(document.getElementById("gameCanvas"), false);
-  game.changeScreen(TestScreen);
+  game.changeScreen(Game.Screens.Splash);
   game.play();
 });
