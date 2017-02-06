d71cdf7b909f14b7c362ccd3b403564bf7b67685
DAB0mB <emanor6@gmail.com>
2017-02-06 05:00:20 -0200

    Step 7.9: Add chaining method to CPP utils

--- resources/cpp/src/utils.h
+++ resources/cpp/src/utils.h
@@ -3,6 +3,30 @@
 #include <string>
 
 namespace utils {
+  template<typename T>
+  class Chain {
+  private:
+    T _accumulator;
+
+  public:
+    Chain(T accumulator);
+
+    Chain<double>* mod(double num);
+
+    Chain<double>* trim(int decimals, const std::string mode = "round");
+
+    Chain<bool>* isBetween(double num1, double num2, const std::string precision = "exact");
+
+    Chain<bool>* compare(double num, const std::string precision = "exact");
+
+    Chain<bool>* compare(double num, const std::string method, const std::string precision);
+
+    T result();
+  };
+
+  template<typename T>
+  Chain<T>* chain(T accumulator);
+
   double mod(double context, double num);
 
   double trim(double context, int decimals, const std::string mode = "round");
