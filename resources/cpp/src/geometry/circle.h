8b6acbe41e51e5c83091da71a4015a3597ef24b0
DAB0mB <emanor6@gmail.com>
2017-02-06 05:44:10 -0200

    Step 7.20: EMBind CPP circle

--- resources/cpp/src/geometry/circle.h
+++ resources/cpp/src/geometry/circle.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <vector>
+#include <emscripten/val.h>
 #include "../nullable.h"
 #include "point.h"
 #include "line.h"
 
 namespace geometry {
   class Line;
+  class EMLine;
 
   class Circle {
   public:
@@ -31,5 +33,22 @@ namespace geometry {
     Nullable<std::vector<Point>> getIntersection(Circle circle);
 
     Nullable<std::vector<Point>> getIntersection(Line line);
+  };
+
+  class EMCircle : public Circle {
+  public:
+    using Circle::Circle;
+
+    emscripten::val getMatchingX(double y);
+
+    emscripten::val getMatchingY(double x);
+
+    emscripten::val getMatchingPoint(double rad);
+
+    emscripten::val getMatchingRad(double x, double y);
+
+    emscripten::val getIntersection(EMLine line);
+
+    emscripten::val getIntersection(EMCircle circle);
   };
 }
